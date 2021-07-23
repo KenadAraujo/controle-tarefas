b411f8dbd78179bc9642134555346c0116d19ed0
Kenad Araujo <kenad@sefaz.pi.gov.br>
2021-07-23 11:53:32 -0300

    -Projeto inicial

---
+++ src/test/java/br/gov/pi/sefaz/controletarefas/ControleTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.pi.sefaz.controletarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
